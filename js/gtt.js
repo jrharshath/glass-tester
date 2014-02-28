51842a79ccbae31090588ca79ebee46183acbd78
Harshath <harshath.jr@gmail.com>
2014-02-28 13:37:46 -0500

    make the cards draggable, and some other improvements

--- js/gtt.js
+++ js/gtt.js
@@ -1,16 +1,63 @@
 $(function() {
 	var W = null;
+
 	$('#start-testing').click(function() {
-		W = window.open("child.html", "Display", "height=500,width=500");
+		var $this = $(this);
+		if($this.hasClass('test-running')) { 
+			W.close();
+		} else {
+			W = window.open("child.html", "Display", "height=500,width=500");
+			$this.addClass('test-running').text('Stop Testing');
+			$('#card-list').addClass('test-running');
+
+			W.onbeforeunload = function() { 
+				$('#start-testing').removeClass('test-running').text('Start Testing');
+				$('#card-list').removeClass('test-running');
+				W = null;
+			};
+		}
 	});
 
 	$('#card-list li h2').click(function() {
-		console.log('showing card');
 		if(W !== null) { 
-			console.log('W not null');
 			var cardclone = $(this).closest('li').clone();
 			cardclone.find('h2').remove();
 			W.showCard(cardclone);
 		}
 	})
+
+	$('#card-list li').drags({handle:'h2', cb: function() {
+		var $li = $(this).closest('li');
+		var id = $li.attr('id');
+
+		var pos = $li.css('top') + "," + $li.css('left');
+		$.cookie(id+"_pos", pos);
+	}});
+
+	$('#card-list li').each(function(i, li) {
+		
+		var cookiename = $(li).attr('id') + "_pos";
+		var pos = $.cookie(cookiename);
+
+		if(pos !== undefined) {
+			var posarr = pos.split(',');
+			$(li).css({
+				position: 'relative',
+				top: posarr[0],
+				left: posarr[1],
+				'z-index': 'auto'
+			});
+		}		
+	});
+
+	$("#clear-positions").click(function() {
+		$('#card-list li').each(function(i, li) {
+			$.removeCookie( $(li).attr('id')+"_pos" );
+		});
+
+		if(W !== null) {
+			W.close();
+		}
+		window.location.reload();
+	});
 });
